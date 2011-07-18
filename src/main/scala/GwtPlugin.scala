ae2e9850d5dfaf2e915b55010a2ba6be6d3873fe
Michael Bayne <mdb@samskivert.com>
2011-07-17 19:26:47 -0700

    Added support for gwt-devmode, modified task depends.
    gwt-compile was previously added as a dependent of prepare-webapp, but
    prepare-webapp must be run before gwt-devmode and clearly one does not want to
    run a long, painful gwt-compile before every gwt-devmode invocation.
    
    Instead now gwt-compile and gwt-devmode depend on prepare-webapp, which makes
    more sense, and package-war depends on gwt-compile. The one caveat is that if
    you use run-jetty, it does not automatically call gwt-compile for you. However,
    this is not totally crazy, since you might want to run-jetty once, and then
    gwt-compile multiple times if you are crazy enough to be edit/compile/debugging
    via gwt-compile instead of gwt-devmode.
    
    I also changed gwt-compile to place its build results directly into
    target/webapp because that is how the GWT compiler works in every other build
    system I've seen it wired into, and it makes it more consistent with
    gwt-devmode which necessarily must be told about target/webapp.
    
    Lastly, added gwt-set-module which is used to tell gwt-devmode which module to
    use, in the event that a project has multiple modules. In the absence of
    specific configuration, the first module found is used.

--- src/main/scala/GwtPlugin.scala
+++ src/main/scala/GwtPlugin.scala
@@ -10,14 +10,31 @@ object GwtPlugin extends Plugin {
   lazy val Gwt = config("gwt") extend (Compile)
 
   val gwtModules = TaskKey[Seq[String]]("gwt-modules")
-  val gwtTemporaryPath = SettingKey[File]("gwt-temporary-path")
   val gwtCompile = TaskKey[Unit]("gwt-compile", "Runs the GWT compiler")
+  val gwtDevMode = TaskKey[Unit]("gwt-devmode", "Runs the GWT devmode shell")
   val gwtVersion = SettingKey[String]("gwt-version")
+
+  var gwtModule: Option[String] = None
+  val gwtSetModule = Command.single("gwt-set-module") { (state, arg) =>
+    Project.evaluateTask(gwtModules, state) match {
+      case Some(Value(mods)) => {
+        gwtModule = mods.find(_.toLowerCase.contains(arg))
+        gwtModule match {
+          case Some(m) => println("gwt-devmode will run: " + m)
+          case None => println("No match for '" + arg + "' in " + mods.mkString(", "))
+        }
+      }
+      case _ => None
+    }
+    state
+  }
 
   lazy val gwtSettings: Seq[Setting[_]] = webSettings ++ gwtOnlySettings
 
   lazy val gwtOnlySettings: Seq[Setting[_]] = inConfig(Gwt)(Defaults.configSettings) ++ Seq(
-    managedClasspath in Gwt <<= (managedClasspath in Compile, update) map { (cp, up) => cp ++ Classpaths.managedJars(Provided, Set("src"), up) },
+    managedClasspath in Gwt <<= (managedClasspath in Compile, update) map {
+      (cp, up) => cp ++ Classpaths.managedJars(Provided, Set("src"), up)
+    },
     unmanagedClasspath in Gwt <<= (unmanagedClasspath in Compile).identity,
     autoScalaLibrary := false,
     gwtVersion := "2.3.0",
@@ -27,20 +44,40 @@ object GwtPlugin extends Plugin {
       "javax.validation" % "validation-api" % "1.0.0.GA" % "provided" withSources (),
       "com.google.gwt" % "gwt-servlet" % gwtVersion)),
     gwtModules <<= (javaSource in Compile) map { javaSource => findGwtModules(javaSource) },
-    gwtTemporaryPath <<= (target) { (target) => target / "gwt-temp" },
-    gwtCompile <<= (dependencyClasspath in Gwt, javaSource in Compile, gwtModules, gwtTemporaryPath, streams) map
-      { (dependencyClasspath, javaSource, gwtModules, tempPath, s) =>
-        {
-          IO.createDirectory(tempPath)
-          val cp = dependencyClasspath.map(_.data.absolutePath) :+ javaSource.absolutePath
-          val command = "java -cp " + cp.mkString(File.pathSeparator) + " com.google.gwt.dev.Compiler -war " + tempPath.absolutePath + " " + gwtModules.mkString(" ")
-          s.log.info("Compiling " + gwtModules.length + " GWT module" + { if (gwtModules.length == 1) "" else "s" } + ": " + gwtModules.mkString(","))
-          s.log.debug("Running GWT compiler command: " + command)
-          command !
-        }
-      },
-    prepareWebapp <<= prepareWebapp.dependsOn(gwtCompile),
-    webappResources <<= (webappResources, gwtTemporaryPath) { (w: PathFinder, g: File) => w +++ PathFinder(g) })
+
+    gwtDevMode <<= (dependencyClasspath in Gwt, javaSource in Compile,
+                    gwtModules, temporaryWarPath, streams) map {
+      (dependencyClasspath, javaSource, gwtModules, warPath, s) => {
+        val module = gwtModule.getOrElse(gwtModules.head)
+        val cp = dependencyClasspath.map(_.data.absolutePath) :+ javaSource.absolutePath
+        val command = mkGwtCommand(cp, "com.google.gwt.dev.DevMode", warPath, module)
+        s.log.info("Running GWT devmode on: " + module)
+        s.log.debug("Running GWT devmode command: " + command)
+        command !
+      }
+    },
+    gwtDevMode <<= gwtDevMode.dependsOn(prepareWebapp),
+
+    gwtCompile <<= (dependencyClasspath in Gwt, javaSource in Compile,
+                    gwtModules, temporaryWarPath, streams) map {
+      (dependencyClasspath, javaSource, gwtModules, warPath, s) => {
+        val cp = dependencyClasspath.map(_.data.absolutePath) :+ javaSource.absolutePath
+        val command = mkGwtCommand(
+          cp, "com.google.gwt.dev.Compiler", warPath, gwtModules.mkString(" "))
+        s.log.info("Compiling GWT modules: " + gwtModules.mkString(","))
+        s.log.debug("Running GWT compiler command: " + command)
+        command !
+      }
+    },
+    gwtCompile <<= gwtCompile.dependsOn(prepareWebapp),
+    packageWar <<= packageWar.dependsOn(gwtCompile),
+
+    commands ++= Seq(gwtSetModule)
+  )
+
+  private def mkGwtCommand(cp: Seq[File], clazz: String, warPath: File, args: String) =
+    val command = "java -cp " + cp.mkString(File.pathSeparator) + " " + clazz +
+      " -war " + warPath.absolutePath + " " + args
 
   private def findGwtModules(srcRoot: File): Seq[String] = {
     import Path.relativeTo
